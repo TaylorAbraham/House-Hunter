86040341f9b4e5b6c8927ea4ead9a9cad863b961
Ryan Abraham <Ryan_JA@live.ca>
2020-01-11 13:07:08 -0500

    Add server index route at /

--- server/server.py
+++ server/server.py
@@ -1,1 +1,16 @@
 from flask import Flask
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def index():
+    print("Request received at /")
+    response = { 'response': 'success' }
+    print("Response sent")
+    return "" + json.dumps(response) + ""
+
+if __name__ == "__main__":
+    app.run()
